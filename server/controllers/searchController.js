e2b9fe51689d533d869cc3bf27603a15356fab37
Marco <marcocayetanojr@gmail.com>
2020-04-07 14:12:45 -0700

    node-4-afternoon-done

---
+++ server/controllers/searchController.js
@@ -0,0 +1,10 @@
+const swag = require("../models/swag");
+
+module.exports = {
+    search: (req, res) => {
+        const { category } = req.query;
+        if (!category) {
+            res.status(200).send(filteredSwag);
+        }
+    }
+};
